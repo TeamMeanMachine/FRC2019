328eda89ae2efecce31b7ac0e8512e9df6147a95
tylerscheuble <tyler@scheuble.us>
2019-02-10 21:19:39 -0800

    clean up a bit

--- src/main/kotlin/org/team2471/frc2019/Main.kt
+++ src/main/kotlin/org/team2471/frc2019/Main.kt
@@ -19,33 +19,15 @@ object Robot: RobotProgram {
     override suspend fun teleop() {
         println("Finished")
         periodic {
-//            Armavator.setArmSetpoint((OI.rightYStick * 45).degrees)
-//            Armavator.setArmRaw(OI.rightYStick)
+            // TODO: move this drive thing into Drive.default()
             Drive.drive(OI.driveTranslation, OI.driveRotation)
-//            println("Current pos: ${OB1.angle}, Setpoint: ${ob1Setpoint}, Output: ${OB1.pivotMotors.output}, Error: ${OB1.pivotMotors.closedLoopError}, Current: ${OB1.pivotMotors.current}")
-//            OB1.pivotRaw(OI.rightYStick * 0.25)
         }
     }
 
     override suspend fun test() {
-//        var setpoint = Armavator.height
-        periodic {
-//            setpoint += (OI.driveTranslationY * 4).inches * period
-//            Armavator.elevate(setpoint)
-        println(compressor.pressureSwitchValue)
-        }
     }
 
-    private val compressor = Compressor()
-
     override suspend fun disable() {
-
-        periodic {
-//            println(Armavator.angle.asDegrees)
-//            println(Armavator.height)
-//            println(OB1.pivotMotors.position)
-//        println(OB1.angle.asDegrees)
-        }
     }
 }
 
