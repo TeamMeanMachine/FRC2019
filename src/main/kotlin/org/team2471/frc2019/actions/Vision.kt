8d622b741c8854949b1c88321f30244d7defa198
bobcroucher <bobcroucher@gmail.com>
2019-03-24 14:19:44 -0700

    power cap for vision

--- src/main/kotlin/org/team2471/frc2019/actions/Vision.kt
+++ src/main/kotlin/org/team2471/frc2019/actions/Vision.kt
@@ -6,27 +6,35 @@ import org.team2471.frc.lib.framework.use
 import org.team2471.frc.lib.math.Vector2
 import org.team2471.frc.lib.motion.following.drive
 import org.team2471.frc.lib.motion.following.stop
+import org.team2471.frc.lib.units.asRadians
+import org.team2471.frc.lib.units.degrees
 import org.team2471.frc2019.Drive
 import org.team2471.frc2019.Jevois
 
-const val DISTANCE_OFFSET = 30 //inches
-const val ANGLE_OFFSET = 4 //degrees
+const val DISTANCE_OFFSET = 18 //inches
+const val ANGLE_OFFSET = 5 //degrees
 const val SKEW_OFFSET = 5 //degrees
 
 suspend fun driveToTarget() = use(Drive, Jevois, name = "Drive To Target") {
     Jevois.isLightEnabled = true
-    val turnController = PDController(0.0125, 8.0)
-    val distanceController = PDController(0.01, 0.02)
+    val turnController = PDController(0.01, 6.0)
+    val distanceController = PDController(0.008, 0.02)
     val strafeController = PDController(0.005, 0.0)
 
     periodic {
         val target = Jevois.target ?: return@periodic Drive.stop()
-        val turn = turnController.update(target.angle.asDegrees - ANGLE_OFFSET)
-        val distance = distanceController.update(target.distance.asInches - DISTANCE_OFFSET) + 0.1
+        val turnError = target.angle - ANGLE_OFFSET.degrees
+        var turn = turnController.update(turnError.asDegrees)
+        val distance = distanceController.update(target.distance.asInches - DISTANCE_OFFSET) + 0.0
         val strafe = strafeController.update(target.skew.asDegrees - SKEW_OFFSET)
 
         println(target)
-        Drive.drive(Vector2(0.0, distance), turn, false)
 
+        val powerCap = 0.2
+        val x = Math.min(Math.max(turnError.sin()*distance, -powerCap), powerCap)
+        val y = Math.min(Math.max(turnError.cos()*distance, 0.0), powerCap)
+        turn = Math.min(Math.max(turn, -powerCap), powerCap)
+
+        Drive.drive(Vector2(x, y), turn, false)
     }
 }
